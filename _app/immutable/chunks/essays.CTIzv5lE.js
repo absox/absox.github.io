c430b72a4f5c844f448f4ac5d01240570c4ea8d8
Ran Liu <xabsox@gmail.com>
2024-11-02 14:12:42 -0700

    Updates

---
+++ _app/immutable/chunks/essays.CTIzv5lE.js
@@ -0,0 +1,17 @@
+import{j as f,h as d,i as o,n as v,L as y,o as r,P as h,ac as g,ad as b,s as p,M as S}from"./runtime.CihvSwKW.js";import{b as l,c as M}from"./disclose-version.6CFbjw_q.js";function w(m,c,j,k,C){var n=m,a="",t;f(()=>{if(a===(a=c()??"")){d&&o();return}t!==void 0&&(y(t),t=void 0),a!==""&&(t=v(()=>{if(d){r.data;for(var i=o(),s=i;i!==null&&(i.nodeType!==8||i.data!=="");)s=i,i=h(i);if(i===null)throw g(),b;l(r,s),n=p(i);return}var u=a+"",e=M(u);l(S(e),e.lastChild),n.before(e)}))})}const R=`- title: <i>King Lear</i> and Kurosawa's <i>Ran</i> on the Banality of Virtue
+  date: 2021-12-29
+  id: lear
+  md: lear.md
+- title: On the Morality of <i>Nirvana in Fire</i>
+  date: 2021-03-27
+  id: langyabang
+  md: langyabang.md
+- title: "Humanity in <i>Shin Sekai Yori</i>: Defined by Emotion, Empathy, and the Subconscious Mind"
+  date: 2019-12-30
+  id: ssy
+  md: ssy.md
+- title: Collectivism and Individualism, Progress and Regress, Societal Trajectory and the Individual Will in Science Fiction
+  date: 2019-04-30
+  id: collectivism
+  md: collectivism.md
+`;export{R as e,w as h};
---
+++ _app/immutable/entry/start.Dwwm64ir.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.DaA1H8lh.js";export{t as start};
